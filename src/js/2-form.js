f14807ed6c0e9fe419cd1e5e314c84241b923bad
Valentyn <manilchuk.v@gmail.com>
2026-02-28 15:39:18 +0200

    executed HW2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
-console.log('Form');
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', e => {
+  const fd = new FormData(form);
+  formData.email = fd.get('email');
+  formData.message = fd.get('message');
+  saveToLS('feedback-form-state', formData);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const userForm = loadFromLS('feedback-form-state', {});
+  form.elements.email.value = userForm.email || '';
+  form.elements.message.value = userForm.message || '';
+  formData.email = userForm.email || '';
+  formData.message = userForm.message || '';
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    console.log('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData = { email: '', message: '' };
+});
+
+function saveToLS(key, value) {
+  const json = JSON.stringify(value);
+  localStorage.setItem(key, json);
+}
+
+function loadFromLS(key, defaultValue) {
+  const jsonData = localStorage.getItem(key);
+  try {
+    const data = JSON.parse(jsonData);
+    return data ?? defaultValue;
+  } catch {
+    return jsonData ?? defaultValue;
+  }
+}
